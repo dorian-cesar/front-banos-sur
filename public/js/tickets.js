87e96a6b2a8cdd4d1767b53410f21aacfa0c0c7c
Diegowigo <dwigodski@gmail.com>
2026-06-03 14:42:32 -0400

    fix: se corrige envio de metodo de pago en tarjeta y se agrega mejor logica para encontrar servicio vendido

--- public/js/tickets.js
+++ public/js/tickets.js
@@ -524,7 +524,14 @@ async function continuarConPago(metodoPago) {
         : "";
 
       console.log(`📤 Enviando a callApi - Código: ${Codigo}`);
-      await callApi({ Codigo, hora, fecha, tipo, valor: precioFinal });
+      await callApi({
+        Codigo,
+        hora,
+        fecha,
+        tipo,
+        valor: precioFinal,
+        medio_pago: metodoPago,
+      });
 
       console.log(`📊 Registrando movimiento en caja - Código: ${Codigo}`);
       await registrarMovimientoCaja({
@@ -1048,9 +1055,9 @@ async function registrarMovimientoCaja(datos) {
     const serviciosDisponibles =
       JSON.parse(localStorage.getItem("serviciosDisponibles")) || {};
 
-    const servicio = Object.values(serviciosDisponibles).find(
-      (s) => s.nombre.toLowerCase() === tipo.toLowerCase(),
-    );
+    const servicio = Object.entries(serviciosDisponibles).find(
+      ([key]) => key.toLowerCase() === tipo.toLowerCase(),
+    )?.[1];
 
     const id_servicio = servicio ? servicio.id : null;
 
